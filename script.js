3d9bd63eadff0912aa180057a1b916dd5001ee7f
Azam <197414196+AlphaPinecone@users.noreply.github.com>
2025-11-27 15:27:37 +0000

    added simple ui functions to play a round

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const rockbtn = document.querySelector(".rock");
+const paperbtn = document.querySelector(".paper");
+const scissorsbtn = document.querySelector(".scissors");
+
 function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * 3);
   if (computerChoice === 0) {
@@ -11,23 +15,25 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-//gets human choice
-function getHumanChoice() {
-  let humanChoice = prompt(
-    "please enter your choice of Rock, Paper or Scissors"
-  );
+//gets human choice with buttons
+rockbtn.addEventListener("click", () => {
+  playRound("ROCK");
+});
 
-  //converts to upper case
-  return humanChoice.toUpperCase();
-}
+paperbtn.addEventListener("click", () => {
+  playRound("PAPER");
+});
+
+scissorsbtn.addEventListener("click", () => {
+  playRound("SCISSORS");
+});
 
 //scoreboard
 let humanScore = 0;
 let computerScore = 0;
 
 //play round
-function playRound() {
-  const humanChoice = getHumanChoice();
+function playRound(humanChoice) {
   const computerChoice = getComputerChoice();
   if (humanChoice === computerChoice) {
     console.log("It's a draw");
@@ -53,17 +59,17 @@ function playRound() {
 }
 
 //play game or 5 rounds
-function playGame() {
-  let i = 0;
-  while (i < 5) {
-    playRound();
-    i++;
-  }
-}
-playGame();
+// function playGame() {
+//   let i = 0;
+//   while (i < 5) {
+//     playRound();
+//     i++;
+//   }
+// }
+// playGame();
 
-function Winner() {
-  if (humanScore > computerScore) console.log("You Win!! WOO");
-  else console.log("loser");
-}
-Winner();
+// function Winner() {
+//   if (humanScore > computerScore) console.log("You Win!! WOO");
+//   else console.log("loser");
+// }
+// Winner();
